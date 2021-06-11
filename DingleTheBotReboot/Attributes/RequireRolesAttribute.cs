b181e7b338059cb5cfd08e27aa97fd2db38d70ea
lochidev <starcrasher54@gmail.com>
2021-06-11 14:32:34 +0530

    making git happy

---
+++ DingleTheBotReboot/Attributes/RequireRolesAttribute.cs
@@ -0,0 +1,19 @@
+﻿using DSharpPlus.SlashCommands;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DingleTheBotReboot.Attributes
+{
+    public class RequireRolesAttribute : SlashCheckBaseAttribute
+    {
+        private readonly string[] Roles;
+        public RequireRolesAttribute(params string[] roles)
+        {
+            Roles = roles;
+        }
+        public override Task<bool> ExecuteChecksAsync(InteractionContext ctx)
+        {
+            return Task.FromResult(ctx.Member.Roles.Any(x => Roles.Any(y => x.Name == y)));
+        }
+    }
+}
--- DingleTheBotReboot/Commands/BasicSlashCommands.cs
+++ DingleTheBotReboot/Commands/BasicSlashCommands.cs
@@ -1,4 +1,5 @@
-﻿using DSharpPlus;
+﻿using DingleTheBotReboot.Attributes;
+using DSharpPlus;
 using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
 using System.Threading.Tasks;
@@ -32,7 +33,7 @@ namespace DingleTheBotReboot.Commands
             await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(embed.Build()));
         }
         [SlashCommand("mute", "Mute the offender :o")]
-        public async Task Mute(InteractionContext ctx, [Option("user", "The user to get it for")] DiscordUser user, [Option("reason", "Reason for mute")] string reason = null)
+        public async Task Mute(InteractionContext ctx, [Option("user", "The user to mute")] DiscordUser user, [Option("reason", "Reason for mute")] string reason = null)
         {
             await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
             if (user is null)
@@ -47,6 +48,31 @@ namespace DingleTheBotReboot.Commands
             .WithFooter($"Requested by {ctx.Member.DisplayName}", ctx.Member.AvatarUrl)
             .WithAuthor($"{user.Username}", user.AvatarUrl, user.AvatarUrl)
             .WithDescription(reason is not null ? $"With reason: {reason}" : "No reason provided");
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(embed.Build()));
+        }
+        [SlashCommand("ban", "Totally Real Ban")]
+        [RequireRoles("RADmin")]
+        public async Task Ban(InteractionContext ctx, [Option("user", "The user to ban ;)")] DiscordUser user = null)
+        {
+            await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
+            if (user is null)
+            {
+                DiscordEmbedBuilder emptyUserembed = new DiscordEmbedBuilder
+                {
+                    Title = $"Ban who",
+                    ImageUrl = "https://i.pinimg.com/originals/04/cc/38/04cc3802ec5fd9b3655f47e488be3a91.gif"
+                }
+                .WithFooter($"Requested by {ctx.Member.DisplayName}", ctx.Member.AvatarUrl)
+                .WithAuthor($"{user.Username}", user.AvatarUrl, user.AvatarUrl);
+                await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(emptyUserembed.Build()));
+            }
+            DiscordEmbedBuilder embed = new DiscordEmbedBuilder
+            {
+                Title = $"Banned user {user.Username}",
+                ImageUrl = "https://i.pinimg.com/originals/75/cd/f6/75cdf69a0efeaa22fe85fe66254e4330.gif"
+            }
+            .WithFooter($"Requested by {ctx.Member.DisplayName}", ctx.Member.AvatarUrl)
+            .WithAuthor($"{user.Username}", user.AvatarUrl, user.AvatarUrl);
             await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(embed.Build()));
         }
     }
