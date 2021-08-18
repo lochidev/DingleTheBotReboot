f4dd040d3511c71674d3711b16a0404680e58505
lochidev <starcrasher54@gmail.com>
2021-08-18 15:27:56 +0530

    stuff :D

--- DingleTheBotReboot/Extensions/HostBuilderExtensions.cs
+++ DingleTheBotReboot/Extensions/HostBuilderExtensions.cs
@@ -32,11 +32,23 @@ namespace DingleTheBotReboot.Extensions
                 appConfig.Sources.Clear();
                 appConfig.AddConfiguration(config);
             });
-
+            string botToken = null;
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                botToken = config["Discord:BotToken"];
+            }
+            else
+            {
+                botToken = Environment.GetEnvironmentVariable("BOT_TOKEN");
+            }
+            if(botToken is null)
+            {
+                throw new Exception("Bot token not found");
+            }
             builder.ConfigureServices(serviceCollection =>
             {
                 serviceCollection
-                    .AddDiscordGateway(_ => config["Discord:BotToken"])
+                    .AddDiscordGateway(_ => botToken)
                     .AddDiscordCommands(true);
                 configure.Invoke(new BotHostBuilder(config, serviceCollection));
                 serviceCollection
