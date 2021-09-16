7fa36f29e6142f7a740f19ce52b178880a9ad1d4
lochidev <starcrasher54@gmail.com>
2021-09-16 08:56:06 +0530

    tiny fixes

--- restart_dingle.sh
+++ restart_dingle.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-BOT_PROCESS=$(bash getbotprocess.sh DingleTheBotReboot.dll)
+BOT_PROCESS=$(bash getbotprocess.sh DingleTheBotReboot)
 kill -9 $BOT_PROCESS
 printf "Killed bot with pId $BOT_PROCESS\nEnter bot token\n"
 read BOT_TOKEN
@@ -17,4 +17,4 @@ export Cosmos:AccountEndpoint=$CDB
 cd mystuff/dinglerebooted/publish
 chmod +x DingleTheBotReboot
 nohup ./DingleTheBotReboot >/dev/null 2>&1 &
-ps aux | grep dotnet
+ps aux | grep Dingle
